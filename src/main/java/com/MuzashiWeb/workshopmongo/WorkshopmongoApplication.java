cccee77ab874cbc0b13dba50e1924fb78cb5532c
Muzashi <evertoncosta100@gmail.com>
2021-09-28 08:29:05 -0300

    Entity User e REST funcionando

--- src/main/java/com/MuzashiWeb/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/MuzashiWeb/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.MuzashiWeb.course;
+package com.MuzashiWeb.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
