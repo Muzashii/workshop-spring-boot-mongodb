e510922ea21b947d8acc9c5ea30d41b7f7b5deec
Muzashi <evertoncosta100@gmail.com>
2021-09-30 09:31:04 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/MuzashiWeb/workshopmongo/domain/Post.java
+++ src/main/java/com/MuzashiWeb/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.MuzashiWeb.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.MuzashiWeb.workshopmongo.dto.AuthorDTO;
+import com.MuzashiWeb.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -74,6 +79,15 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -91,6 +105,5 @@ public class Post implements Serializable{
 		return Objects.equals(id, other.id);
 	}
 
-	
-	
+
 }
